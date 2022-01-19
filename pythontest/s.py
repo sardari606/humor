5d2fceb6277025f7e12c42aed6315255ab42be7c
HSD <42543226+sardari606@users.noreply.github.com>
2022-01-19 20:29:12 +0330

    Update s.py

--- pythontest/s.py
+++ pythontest/s.py
@@ -4,6 +4,6 @@ def handler(sig, frame):
  print(var)
 signal.signal(signal.SIGALRM, handler) #register alarm to function
 var = input("pls put the input: ")
-while i != 0:
+while 1:
  signal.alarm(1)
  signal.pause()
